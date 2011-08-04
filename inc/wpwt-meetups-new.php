4ec0be94f29189d6861eeaf7adc1c92fb2bca590
todsul <todsul@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-04 06:08:50 +0000

    * Released 1.1.1
    git-svn-id: https://plugins.svn.wordpress.org/wp-world-travel/tags/1.1.1@419085 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/wpwt-meetups-new.php
@@ -0,0 +1,7 @@
+<div id="update-nag" class="update-nag" style="margin:0 auto;width:75%">
+	You have 
+	<span style="color:red">NEW</span> 
+	Meetup Proposals! 
+	<a href="<?php echo WPWT_MEETUPS ?>">View Meetups</a> 
+	to close this message.
+</div>
