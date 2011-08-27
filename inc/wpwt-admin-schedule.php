6bb6dde4b96b0cb762b0bd0cf8aa1ba891bf065e
todsul <todsul@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-27 05:16:59 +0000

    * Release 1.1.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-world-travel/tags/1.1.2@429323 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/wpwt-admin-schedule.php
@@ -0,0 +1,111 @@
+<?php
+$countries = array( 'AF'=>'Afghanistan', 'AX'=>'Aland Islands', 'AL'=>'Albania', 'DZ'=>'Algeria', 'AS'=>'American Samoa', 'AD'=>'Andorra', 'AO'=>'Angola', 'AI'=>'Anguilla', 'AQ'=>'Antarctica', 'AG'=>'Antigua And Barbuda', 'AR'=>'Argentina', 'AM'=>'Armenia', 'AW'=>'Aruba', 'AU'=>'Australia', 'AT'=>'Austria', 'AZ'=>'Azerbaijan', 'BS'=>'Bahamas', 'BH'=>'Bahrain', 'BD'=>'Bangladesh', 'BB'=>'Barbados', 'BY'=>'Belarus', 'BE'=>'Belgium', 'BZ'=>'Belize', 'BJ'=>'Benin', 'BM'=>'Bermuda', 'BT'=>'Bhutan', 'BO'=>'Bolivia', 'BQ'=>'Bonaire', 'BA'=>'Bosnia And Herzegovina', 'BW'=>'Botswana', 'BV'=>'Bouvet Island', 'BR'=>'Brazil', 'IO'=>'British Indian Ocean Territory', 'BN'=>'Brunei Darussalam', 'BG'=>'Bulgaria', 'BF'=>'Burkina Faso', 'BI'=>'Burundi', 'KH'=>'Cambodia', 'CM'=>'Cameroon', 'CA'=>'Canada', 'CV'=>'Cape Verde', 'KY'=>'Cayman Islands', 'CF'=>'Central African Republic', 'TD'=>'Chad', 'CL'=>'Chile', 'CN'=>'China', 'CX'=>'Christmas Island', 'CC'=>'Cocos Islands', 'CO'=>'Colombia', 'KM'=>'Comoros', 'CG'=>'Congo', 'CD'=>'Congo DR', 'CK'=>'Cook Islands', 'CR'=>'Costa Rica', 'CI'=>'Cote D\'ivoire', 'HR'=>'Croatia', 'CU'=>'Cuba', 'CW'=>'Curacao', 'CY'=>'Cyprus', 'CZ'=>'Czech Republic', 'DK'=>'Denmark', 'DJ'=>'Djibouti', 'DM'=>'Dominica', 'DO'=>'Dominican Republic', 'EC'=>'Ecuador', 'EG'=>'Egypt', 'SV'=>'El Salvador', 'GQ'=>'Equatorial Guinea', 'ER'=>'Eritrea', 'EE'=>'Estonia', 'ET'=>'Ethiopia', 'FK'=>'Falkland Islands', 'FO'=>'Faroe Islands', 'FJ'=>'Fiji', 'FI'=>'Finland', 'FR'=>'France', 'GF'=>'French Guiana', 'PF'=>'French Polynesia', 'GA'=>'Gabon', 'GM'=>'Gambia', 'GE'=>'Georgia', 'DE'=>'Germany', 'GH'=>'Ghana', 'GI'=>'Gibraltar', 'GR'=>'Greece', 'GL'=>'Greenland', 'GD'=>'Grenada', 'GP'=>'Guadeloupe', 'GU'=>'Guam', 'GT'=>'Guatemala', 'GG'=>'Guernsey', 'GN'=>'Guinea', 'GW'=>'Guinea-Bissau', 'GY'=>'Guyana', 'HT'=>'Haiti', 'HM'=>'Heard Island', 'VA'=>'Vatican City', 'HN'=>'Honduras', 'HK'=>'Hong Kong', 'HU'=>'Hungary', 'IS'=>'Iceland', 'IN'=>'India', 'ID'=>'Indonesia', 'IR'=>'Iran', 'IQ'=>'Iraq', 'IE'=>'Ireland', 'IM'=>'Isle Of Man', 'IL'=>'Israel', 'IT'=>'Italy', 'JM'=>'Jamaica', 'JP'=>'Japan', 'JE'=>'Jersey', 'JO'=>'Jordan', 'KZ'=>'Kazakhstan', 'KE'=>'Kenya', 'KI'=>'Kiribati', 'KP'=>'Korea, North', 'KR'=>'Korea, South', 'KW'=>'Kuwait', 'KG'=>'Kyrgyzstan', 'LA'=>'Laos', 'LV'=>'Latvia', 'LB'=>'Lebanon', 'LS'=>'Lesotho', 'LR'=>'Liberia', 'LY'=>'Libya', 'LI'=>'Liechtenstein', 'LT'=>'Lithuania', 'LU'=>'Luxembourg', 'MO'=>'Macao', 'MK'=>'Macedonia', 'MG'=>'Madagascar', 'MW'=>'Malawi', 'MY'=>'Malaysia', 'MV'=>'Maldives', 'ML'=>'Mali', 'MT'=>'Malta', 'MH'=>'Marshall Islands', 'MP'=>'Mariana Islands', 'MQ'=>'Martinique', 'MR'=>'Mauritania', 'MU'=>'Mauritius', 'YT'=>'Mayotte', 'MX'=>'Mexico', 'FM'=>'Micronesia', 'MD'=>'Moldova', 'MC'=>'Monaco', 'MN'=>'Mongolia', 'ME'=>'Montenegro', 'MS'=>'Montserrat', 'MA'=>'Morocco', 'MZ'=>'Mozambique', 'MM'=>'Myanmar', 'NA'=>'Namibia', 'NR'=>'Nauru', 'NP'=>'Nepal', 'NL'=>'Netherlands', 'NC'=>'New Caledonia', 'NZ'=>'New Zealand', 'NI'=>'Nicaragua', 'NE'=>'Niger', 'NG'=>'Nigeria', 'NU'=>'Niue', 'NF'=>'Norfolk Island', 'NO'=>'Norway', 'OM'=>'Oman', 'PK'=>'Pakistan', 'PW'=>'Palau', 'PS'=>'Palestinian Territory, Occupied', 'PA'=>'Panama', 'PG'=>'Papua New Guinea', 'PY'=>'Paraguay', 'PE'=>'Peru', 'PH'=>'Philippines', 'PN'=>'Pitcairn', 'PL'=>'Poland', 'PT'=>'Portugal', 'PR'=>'Puerto Rico', 'QA'=>'Qatar', 'RE'=>'Reunion', 'RO'=>'Romania', 'RU'=>'Russia', 'RW'=>'Rwanda', 'BL'=>'Saint Barthelemy', 'SH'=>'Saint Helena', 'KN'=>'Saint Kitts And Nevis', 'LC'=>'Saint Lucia', 'MF'=>'Saint Martin', 'PM'=>'Saint Pierre And Miquelon', 'VC'=>'Saint Vincent', 'WS'=>'Samoa', 'SM'=>'San Marino', 'ST'=>'Sao Tome And Principe', 'SA'=>'Saudi Arabia', 'SN'=>'Senegal', 'RS'=>'Serbia', 'SC'=>'Seychelles', 'SL'=>'Sierra Leone', 'SG'=>'Singapore', 'SK'=>'Slovakia', 'SI'=>'Slovenia', 'SB'=>'Solomon Islands', 'SO'=>'Somalia', 'ZA'=>'South Africa', 'GS'=>'South Georgia', 'ES'=>'Spain', 'LK'=>'Sri Lanka', 'SD'=>'Sudan', 'SR'=>'Suriname', 'SJ'=>'Svalbard', 'SZ'=>'Swaziland', 'SE'=>'Sweden', 'CH'=>'Switzerland', 'SY'=>'Syria', 'TW'=>'Taiwan', 'TJ'=>'Tajikistan', 'TZ'=>'Tanzania', 'TH'=>'Thailand', 'TL'=>'Timor-Leste', 'TG'=>'Togo', 'TK'=>'Tokelau', 'TO'=>'Tonga', 'TT'=>'Trinidad And Tobago', 'TN'=>'Tunisia', 'TR'=>'Turkey', 'TM'=>'Turkmenistan', 'TC'=>'Turks And Caicos Islands', 'TV'=>'Tuvalu', 'UG'=>'Uganda', 'UA'=>'Ukraine', 'AE'=>'United Arab Emirates', 'GB'=>'United Kingdom', 'US'=>'United States', 'UY'=>'Uruguay', 'UZ'=>'Uzbekistan', 'VU'=>'Vanuatu', 'VE'=>'Venezuela', 'VN'=>'Viet Nam', 'VG'=>'Virgin Islands, British', 'VI'=>'Virgin Islands, U.S.', 'WF'=>'Wallis And Futuna', 'EH'=>'Western Sahara', 'YE'=>'Yemen', 'ZM'=>'Zambia', 'ZW'=>'Zimbabwe' );
+?>
+
+<div class="wrap">
+	<div id="icon-tools" class="icon32"></div><h2>Travel Schedule</h2>
+	<h3>Add New Location</h3>
+	<form method="post" action="options.php">
+		<?php settings_fields( 'wpwt_schedule' ); ?>
+		<p>
+			<label for="wpwt_schedule[wpwt-from-day]" style="display:inline-block;width:7em">From Date</label>
+			<input size="2" maxlength="2" type="text" name="wpwt_schedule[wpwt-from-day]" value="01" />								
+			<?php
+				$month = "<select  name=\"wpwt_schedule[wpwt-from-month]\">\n";
+				for ( $i = 1; $i < 13; $i = $i +1 ) {
+					$month .= "\t\t\t" . '<option value="' . zeroise( $i, 2 ) . '"';
+					if ( $i == $mm )
+						$month .= ' selected="selected"';
+					$month .= '>' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) . "</option>\n";
+				}
+				$month .= '</select>';
+				
+				echo $month;
+			?>
+			<input size="4" maxlength="4" type="text" name="wpwt_schedule[wpwt-from-year]" value="<?php echo date( 'Y' ); ?>" />
+		</p>
+		<p>
+			<label for="wpwt_schedule[wpwt-to-day]" style="display:inline-block;width:7em">To Date</label>
+			<input size="2" maxlength="2" type="text" name="wpwt_schedule[wpwt-to-day]" value="01" />								
+			<?php
+				$month = "<select  name=\"wpwt_schedule[wpwt-to-month]\">\n";
+				for ( $i = 1; $i < 13; $i = $i +1 ) {
+					$month .= "\t\t\t" . '<option value="' . zeroise( $i, 2 ) . '"';
+					if ( $i == $mm )
+						$month .= ' selected="selected"';
+					$month .= '>' . $wp_locale->get_month_abbrev( $wp_locale->get_month( $i ) ) . "</option>\n";
+				}
+				$month .= '</select>';
+				
+				echo $month;
+			?>
+			<input size="4" maxlength="4" type="text" name="wpwt_schedule[wpwt-to-year]" value="<?php echo date( 'Y' ); ?>" />							
+		</p>
+		<p>
+			<label for="wpwt_schedule[wpwt-place]" style="display:inline-block;width:7em">Place/City</label>
+			<input size="33" maxlength="64" type="text" name="wpwt_schedule[wpwt-place]" />
+		</p>
+		<p>
+			<label for="wpwt_schedule[wpwt-country]" style="display:inline-block;width:7em">Country</label>
+			<select name="wpwt_schedule[wpwt-country]">
+				<?php
+				
+					foreach( $countries as $key=>$value ) {
+					
+						echo '<option value="' . $key . '-' . $value . '">' . $value . '</option>';
+					
+					}
+					
+				?>
+			</select>
+		</p>		
+		<p class="submit">
+			<input type="submit" class="button-primary" value="Add Location" />
+		</p>					
+	</form>
+	<h3>Manage Existing Locations</h3>	
+	<table class="widefat">
+		<thead>
+			<tr>							
+				<th width="20%">From Date</th>
+				<th width="20%">To Date</th>
+				<th width="25%">Place/City</th>
+				<th width="20%">Country</th>
+				<th width="15%">Admin</th>
+			</tr>
+		</thead>
+		<tfoot>
+			<tr>							
+				<th>From Date</th>
+				<th>To Date</th>
+				<th>Place/City</th>
+				<th>Country</th>
+				<th>Admin</th>
+			</tr>
+		</tfoot>
+		<tbody>
+			<?php if( is_array( $options ) ) { ?>
+				<?php array_multisort( $options, SORT_DESC ); ?>
+				<?php foreach( $options as $key=>$value ) { ?>								
+					<tr>
+						<td>
+							<?php echo date_i18n( 'j M Y', $value['wpwt_from_date'] ); ?>				
+						</td>
+						<td>
+							<?php echo date_i18n( 'j M Y', $value['wpwt_to_date'] ); ?>				
+						</td>
+						<td><?php echo $value['wpwt_place']; ?></td>
+						<td><?php echo $value['wpwt_country_name']; ?></td>
+						<td>
+							<form method="post" action="options.php">
+								<?php settings_fields( 'wpwt_schedule' ); ?>
+								<input type="hidden" name="wpwt-delete" value="<?php echo $key; ?>" />
+								<input type="submit" class="button-secondary" value="Delete" />
+							</form>
+						</td>
+					</tr>
+				<?php } ?>
+			<?php } ?>												
+		</tbody>
+	</table>
+</div>
---
+++ readme.txt
@@ -0,0 +1,102 @@
+=== Plugin Name ===
+Contributors: todsul
+Donate link: http://globetrooper.com/
+Tags: current location, travel, travel blog, travel schedule, travel itinerary, meetup, travel partners, backpacking
+Requires at least: 2.8
+Tested up to: 3.3
+Stable tag: 1.1.2
+
+Show your current location and travel schedule in the sidebar of your travel blog. Readers can propose meetups at each of your future destinations.
+
+== Description ==
+
+Home Page: http://globetrooper.com/notes/wordpress-world-travel-plugin/
+
+With WP World Travel, you can do the following:
+
+* Display your Current Location to readers
+* Display your Travel Schedule to readers
+* Allow readers to propose Meetups in each location
+
+The mantra of Globetrooper is to help people travel the world together. And the purpose of the WP World Travel 
+plugin is the same. Of course, as a travel blogger, it also makes sense to display your travel plans and 
+meetup with readers around the globe.
+
+View a LIVE demo of the plugin at http://globetrooper.com/notes/. You'll find the plugin in the sidebar under the 
+heading 'Current Location'. Click the links in the widget to test the full functionality.
+
+
+== Installation ==
+
+1. Upload `wp-world-travel.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Drag the widget into a sidebar through the 'Widgets' menu under 'Appearance' in WordPress
+
+== Frequently Asked Questions ==
+
+= How do I change my Current Location? =
+
+In the WordPress dashboard you will find a menu for Schedule. That is where you can add your travel plans. The Current
+Location is chosen from your future destinations. The plugin finds the location where the 'from' date is BEFORE today
+and the 'to' date is AFTER today.
+
+= How am I notified of proposed Meetups? =
+
+All Meetups are recorded to WordPress so you can find them when you log into the Dashboard. You may also receive 
+an email notification if your server is setup correctly. Also check you spam folders if you are not 
+receiving the emails.
+
+
+== Screenshots ==
+
+1. Sidebar widget with Schedule opened.
+2. Admin for Travel Schedule
+3. Admin for proposed Meetups
+
+
+== Changelog ==
+
+= 1.1.1 =
+* Now able to show previous places visited
+* Now the schedule link is hidden in expanded view
+
+= 1.1.1 =
+* Now able to hide the Meetup link
+* Non-English dates are working properly
+
+= 1.0.8 =
+* Fixed month name to match foreign locale
+
+= 1.0.6 =
+* Fixed widget title bug - you can now edit the title afterwards
+* When you deactivate, the widget is now auto removed from your sidebar
+
+= 1.0.5 =
+* Fixed link bug
+
+= 1.0.4 =
+* Allowed user to specify country only (specific place/city not mandatory)
+
+= 1.0.3 =
+* Fixed default value for text links bug
+* Fixed boolean settings update bug
+
+= 1.0.2 =
+* Check if schedule exists before displaying widget text
+* Localize terms from WP core language files
+* Ability to customise text in widget for terms not in WP core language files
+* Now suitable for WP in other languages
+
+= 1.0.1 =
+* Fixed jQuery conflict
+* Fixed Meetup heading pushing location box to next line
+* Fixed email validation error
+
+= 1.0.0 =
+* This is the first release version.
+
+== Upgrade Notice ==
+
+= 1.1.2 =
+* Schedule intro is now in a <p> tag
+* Make sure spacing between intro and schedule is okay
